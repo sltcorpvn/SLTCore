a8220e52afd2125f20fb800ddbad37ad9cfee243
Yen Truong <truthblue82@gmail.com>
2017-09-22 02:38:48 +0700

    add code to index.js

---
+++ index.js
@@ -0,0 +1,64 @@
+/**
+ * Created by yen.truong-thi-hong on 22/09/2017.
+ */
+global.__base = __dirname + '/';
+var config    = require(__dirname + '/config');
+var express   = require('express');
+var http      = require('http');
+var path      = require('path');
+var fs        = require('fs');
+var session   = require('client-sessions');
+var bodyParser = require('skipper');
+var dbMongo    = require('./models/dbMongo');
+var ENV       = process.env.MODE_ENV || 'default';
+
+
+
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
+var sesOPT = config.session;
+var host = config.http.host;
+var port = config.http.port;
+
+if (app.get('env') === 'production' || app.get('env') === 'development' || app.get('env') === 'staging'){
+    sesOPT.cookie.secure = true;
+}
+
+app.use(session(sesOPT));
+
+/*app.use(busboy({limits: {
+    fileSize: 2 * 1024 * 1024
+}}));*/
+
+app.use(express.static(path.join(__dirname, 'assets')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// dynamically include routes in controllers folder
+fs.readdirSync('./controllers').forEach(function (file) {
+    if(file.substr(-3) == '.js') {
+        route = require('./controllers/' + file);
+        route.controller(app, config, fs);
+    }
+});
+
+var server = http.createServer(app);
+var svrOPT = {
+    headers: {connection: 'keep-alive'},
+    agent  : false,
+    port   : port,
+    host   : host,
+    exclusive: true
+};
+server.timeout = 0;
+process.on('uncaughtException', function(err) {
+    console.log('process.on handler');
+    console.log(err);
+});
+server.listen(port, host, function(err){
+    console.log("Express server listening on port " + host + ':' + port);
+});
+
