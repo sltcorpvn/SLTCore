737360aaa2952f52482d0ed821308d13f8e65e3e
Yen Truong <truthblue82@gmail.com>
2017-10-19 01:46:32 +0700

    add login info

--- api/controllers/LoginController.js
+++ api/controllers/LoginController.js
@@ -40,43 +40,6 @@ module.exports = {
             });
 
         })(req, res);
-        /*var username = req.body.username ? req.body.username : "";
-        var password = req.body.password ? req.body.password : "";
-
-        if(!username || !password){
-            return res.json({status: 0, err: "Username or Password is empty!"});
-        }else{
-            var sess = "";
-            if(req.session)
-                sess = req.session;
-                
-            var backURL = sails.config.sltconfig.url.front.user.profile;
-            if(sess && sess.backURL)
-                backURL = sess.backURL;
-            else sess.backURL = backURL;    
-
-            var curDate = Utils.genDBDate(); 
-            Users.findOne({$and:[
-                                {$or: [{valid_to: null}, {valid_to: {$gte: curDate}}]
-                            },
-                            {username: username}
-                            ]})
-                .then((user) => {
-                    if(!user){
-                        res.json({status: 0, err: "User is not exist!"});
-                    }else{
-                        bcrypt.compare(password, user.password).then(function(rs){
-                            if(!rs){
-                                res.json({status: 0, err: "Password's wrong!"});
-                            }else{
-                                req.session.userId  = user._id;
-                                res.json({status: 1, backURL: backURL});
-                            }
-                        });
-                    }
-                })
-                .catch((err) => res.json({status: 0, err: err})); 
-        }*/
     },
 
     logout: function(req, res){
--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,19 +6,8 @@
 
 module.exports = {
     getProfile: function(req, res){
-        //var sess = req.session.sltcore;
-        //var user = sess.user;
-        //if(sess && user){
-        console.log("line 12 in usercontroller:");
-            /*show profile of user*/
+        
         //res.render("user/profile", {user: user});
         res.render("user/profile");
-        //}else{
-        //    console.log("line 19 in usercontroller");
-            /*store current url for redirect after login*/
-        //    sess.backURL = req.url;
-            /*go to login page*/
-        //    res.redirect( sails.config.sltconfig.url.front.login );
-        //}
     }
 };
